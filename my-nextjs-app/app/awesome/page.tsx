551a92bdde881afc0fd7b63e681d53266dfce9dd
JivkoKarakashev <86545491+JivkoKarakashev@users.noreply.github.com>
2025-11-04 10:22:03 +0200

    add react app

---
+++ my-nextjs-app/app/awesome/page.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+const AwesomePage = (): ReactElement => {
+    return(
+        <main>
+            <h1>NextJS is awesome!</h1>
+        </main>
+    );
+}
+
+export default AwesomePage;
